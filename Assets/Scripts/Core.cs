35dd4d9e973795630f941b876c6d448b033274d2
RobertsMaxwell <max031029@gmail.com>
2019-12-02 17:42:37 -0600

    Basic Button Spawning

---
+++ Assets/Scripts/Core.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Core : MonoBehaviour
+{
+    [SerializeField] GameObject targetButton;
+    [SerializeField] float startSize = 1;
+    [SerializeField] float targetSize = 50;
+    [SerializeField] float instFrequency = 2;
+    [SerializeField] int amountPerInst = 2;
+
+    Vector2 canvasOffset;
+
+    Canvas canvas;
+    float timeLeftUntilInst = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = FindObjectOfType<Canvas>();
+        canvasOffset = canvas.GetComponent<RectTransform>().position;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) ;
+
+        if (timeLeftUntilInst - Time.deltaTime <= 0)
+        {
+            timeLeftUntilInst = instFrequency;
+            InstantiateCircle(startSize, targetSize, amountPerInst);
+        }
+        else
+        {
+            timeLeftUntilInst -= Time.deltaTime;
+        }
+
+    }
+
+    public void InstantiateCircle(float startSize, float desiredSize, int amount)
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            Vector2 position = new Vector2(
+                Random.Range(
+                    -(canvas.GetComponent<RectTransform>().rect.width / 2 - desiredSize / 2),
+                    canvas.GetComponent<RectTransform>().rect.width / 2 - desiredSize / 2)
+                        + canvasOffset.x,
+                Random.Range(
+                    -(canvas.GetComponent<RectTransform>().rect.height / 2 - desiredSize / 2),
+                    canvas.GetComponent<RectTransform>().rect.height / 2 - desiredSize / 2)
+                        + canvasOffset.y
+                );
+
+            if (Physics2D.OverlapCircle(position, desiredSize / 2) == null)
+            {
+                var instTarget = Instantiate(targetButton);
+
+                instTarget.GetComponent<RectTransform>().sizeDelta = new Vector2(startSize, startSize);
+                instTarget.GetComponent<CircleCollider2D>().radius = instTarget.GetComponent<RectTransform>().sizeDelta.x / 2;
+                instTarget.GetComponentsInChildren<CircleCollider2D>()[1].radius = desiredSize / 2;
+                instTarget.transform.SetParent(canvas.transform);
+                instTarget.GetComponent<RectTransform>().position = position;
+                StartCoroutine(instTarget.GetComponent<Target>().Grow(desiredSize));
+            }
+            else
+            {
+                InstantiateCircle(startSize, desiredSize, 1);
+            }
+        }    
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] float timeBetweenChanges = .1f;
+    [SerializeField] float timeWhileLargeMin = .5f;
+    [SerializeField] float timeWhileLargeMax = 1f;
+
+    Vector2 startSize;
+
+    float fractionOfWhole = 0f;
+    bool growing = true;
+    bool canRun = true;
+
+    void Awake()
+    {
+        startSize = gameObject.GetComponent<RectTransform>().sizeDelta;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && GetComponent<CircleCollider2D>().OverlapPoint(Input.mousePosition))
+        {
+            Remove();
+        }
+    }
+
+    public void Remove()
+    {
+        canRun = false;
+        Destroy(gameObject);
+    }
+
+    public IEnumerator Grow(float desiredSize)
+    {
+        fractionOfWhole = startSize.x / desiredSize;
+        while (canRun)
+        {
+            if (growing)
+            {
+                fractionOfWhole += Time.deltaTime;
+                gameObject.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(new Vector2(1, 1), new Vector2(desiredSize, desiredSize), fractionOfWhole);
+                gameObject.GetComponent<CircleCollider2D>().radius = GetComponent<RectTransform>().sizeDelta.x / 2;
+                yield return new WaitForSeconds(timeBetweenChanges);
+
+                if (fractionOfWhole >= 1) 
+                {
+                    growing = false;
+                    yield return new WaitForSeconds(Random.Range(timeWhileLargeMin, timeWhileLargeMax));
+                }
+            }
+            else
+            {
+                fractionOfWhole -= Time.deltaTime;
+                gameObject.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(new Vector2(1, 1), new Vector2(desiredSize, desiredSize), fractionOfWhole);
+                gameObject.GetComponent<CircleCollider2D>().radius = GetComponent<RectTransform>().sizeDelta.x / 2;
+
+                if (fractionOfWhole <= 0)
+                {
+                    Remove();
+                    yield break;
+                }
+                else
+                {
+                    yield return new WaitForSeconds(timeBetweenChanges);
+                }
+            }
+        }
+    }
+}
